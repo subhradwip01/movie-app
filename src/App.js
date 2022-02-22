aa70194506c5974e779c371bc6e9b9e6a4729b12
subhradwip01 <subhradwip01@gmail.com>
2022-02-23 00:21:44 +0530

    navbar component created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./App.css"
+import Navbar from './components/Navbar/Navbar'
 const App = () => {
   return (
-    <div>App</div>
+    <div><Navbar/></div>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import play from "../../assets/play.png"
+import "./Navbar.css"
+const Navbar=() =>{
+  const navMenu=[
+      {
+          "name":"Home",
+          "path":"/"
+      },
+      {
+          "name":"Movies",
+          "path":"movies"
+      },
+      {
+          "name":"TV Series",
+          "path":"/tv"
+      }
+  ]
+  return (
+   <nav>
+       <div className='nav-brand'>
+           <img src={play} alt="logo"/>
+           <p>MovieVerse</p>
+       </div>
+       <div className='nav-menu'>
+           <ul>
+               {navMenu.map((e,i)=>(
+                   <li className='nav-list' >
+                       <NavLink key={i} exact activeClassName="selected" className='nav-list__link' to={e.path}>
+                           {e.name}
+                       </NavLink>
+                   </li>
+               ))}
+           </ul>
+       </div>
+   </nav>
+  )
+}
+
+export default Navbar
