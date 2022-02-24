62cfd27fae2491d014b4a6b8bd55e03625b1ec88
subhradwip01 <subhradwip01@gmail.com>
2022-02-24 23:37:28 +0530

    Button added to hero slider item
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/Heroslider/SliderItem.jsx
+++ src/components/Heroslider/SliderItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IMAGE_PATHO, IMAGE_PATHW } from "../../services/movie-api";
 import "./SlideItem.css";
+import { useHistory } from "react-router-dom";
 
 const SliderItem = ({ id, title, overview, backdrop_path, poster_path }) => {
   const poster_url = `${IMAGE_PATHW}${poster_path}`;
@@ -8,6 +9,10 @@ const SliderItem = ({ id, title, overview, backdrop_path, poster_path }) => {
   const bg = {
     backgroundImage: `url(${backdrop_url})`,
   };
+  const history=useHistory()
+  const seeDeatilshandler=()=>{
+    history.push(`/movie/${id}`)
+  }
 
   return (
     <div className="movie-slide__item" id={`movie_${id}`} style={bg}>
@@ -15,6 +20,8 @@ const SliderItem = ({ id, title, overview, backdrop_path, poster_path }) => {
         <div className="movie-data">
           <h1 className="movie-heading">{title}</h1>
           <p className="movie-desc">{overview}</p>
+          <button className="btn-outline" style={{backgroundColor:"#ff0000",boxShadow:"rgb(255 0 0) -1px 0px 19px 0px" , marginRight:"20px"}}>Watch trailer</button>
+          <button className="btn-outline" onClick={seeDeatilshandler}>See Details</button>
         </div>
         <div className="movie-poster">
           <img className="movie-poster__img" src={poster_url} alt="" />
