3797731007856c395d9d64aa5e0351ebf16c672c
subhradwip01 <subhradwip01@gmail.com>
2022-02-24 23:36:32 +0530

    movie-api refoctoring
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -7,25 +7,32 @@ const API_KEY="2bfaba6b6c952f32467c2d20d08e5d93"
 export const IMAGE_PATHW="https://image.tmdb.org/t/p/w500"
 export const IMAGE_PATHO="https://image.tmdb.org/t/p/original"
 
-export const category={
+export const catlog={
     movie: 'movie',
     tv: 'tv'
 }
 
 export const movieType={
-    popular:"popular"
+    popular:"popular",
+    upcoming:"upcoming",
+    top_rated:"top_rated"
+}
+export const tvType={
+    latest:"latest",
+    popular:"popular",
+    top_rated:"top_rated"
 }
 
 export const movieApi=createApi({
     reducerPath:"movieApi",
     baseQuery: fetchBaseQuery({baseUrl:BASE_URL}),
     endpoints: (builder)=>({
-        getMovies: builder.query({
-            query: (category) => `/movie/${category}?api_key=${API_KEY}`
-        })
+        getMoviesOrTvs: builder.query({
+            query: ({category,type}) => `/${category}/${type}?api_key=${API_KEY}`
+        }),
     })
 })
 
 export const {
-    useGetMoviesQuery
+    useGetMoviesOrTvsQuery,
 }=movieApi
